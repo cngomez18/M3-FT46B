9144902107aed6381167b1c3f4d6c29255da58e2
Candela.Gomez.18 <cn.gomez18@gmail.com>
2024-01-23 13:21:53 -0300

    hw-01

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,7 +2,28 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function bash() {
+   process.stdout.write("prompt > ")
+   
+   process.stdin.on("data",(data)=>{
+      
+      let args = data.toString().trim().split(' ')
+      const cmd = args.shift()
+      args = args.join(' ')
+      
+      if(!commands[cmd]){
+         print(`command not found: ${cmd}`)
+      }else{
+         commands[cmd](print, args);
+      }
+   })
+}
+
+function print(output){
+   process.stdout.write(output)
+   process.stdout.write("\nprompt > ")
+   
+}
 
 bash();
 module.exports = {
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,57 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd())
+}
 
-function date() {}
+function date(print) {
+    print(Date())
+}
 
-function echo() {}
+function echo(print,args) {
+    print(args)
+}
 
-function ls() {}
+function ls(print) {
+    fs.readdir(".",(error,files)=>{
+        if(error) throw "hay un error" 
+        else{ 
+            files= files.join(' ')
+            print(files)}
+        
+    })
+}
 
-function cat() {}
+function cat(print,args) {
+    fs.readFile(args,'utf-8',(error,data)=>{
+        if(error) throw "hay un error"
+        else print(data)
+    })
+}
 
-function head() {}
+function head(print,args) {
+    fs.readFile(args,'utf-8',(error,data)=>{
+        data = data.split('\n').slice(0, 8)
+        if(error) throw "hay un error"
+        else print(data[0])
+    })
+}
 
-function tail() {}
+function tail(print,args) {
+    fs.readFile(args,'utf-8',(error,data)=>{
+        data = data.split(' ')
+        data = data[data.length-1]
+        if(error) throw "hay un error"
+        else print(data)
+    })
+}
 
-function curl() {}
+function curl(print,args) {
+    utils.request(args,(error,response)=>{
+        if(error) throw "hay un error"
+        print(response)
+    })
+}
 
-module.exports = {};
+module.exports = {pwd,date,echo,ls,cat,head,tail,curl};
